847e450375fc6d58be7e7e2ec0675a8a5ea1a867
张宗义 <2293295470@qq.com>
2023-03-22 20:33:44 +0800

    s

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,13 @@ export default class App extends Component {
             <div>
                 <div className={styles.c}>css modules</div>
 
-                <div className='box1' style={{width: 100,height: 100,backgroundColor: 'pink'}}></div>
+                {/* <div className='box1' style={{width: 100,height: 100,backgroundColor: 'pink'}}></div>
 
                 <div className='box2'></div>
 
                 <div className='box3'></div>
 
-                <div className='box4'></div>
+                <div className='box4'></div> */}
 
             </div>
         )
---
+++ src/style/cssInJs.js
@@ -0,0 +1,59 @@
+import styled, { keyframes } from "styled-components"
+
+// 编译成 div 且具有 下面的样式
+const Box1 = styled.div`
+    width: 100px;
+    height: 100px;
+    background: red;
+`
+// 组件嵌套内容
+const Box2 = styled.div`
+    width: 100px;
+    height: 100px;
+    background: pink;
+    p{
+        color: blue;
+    }
+    span{
+        color: yellow;
+        &:hover{
+        color: green;
+        }
+    }
+`
+// 继承
+const Box3 = styled(Box1)`
+    border: 10px solid #00accd;
+`
+// 使用props
+const Box4 = styled.div`
+    width: 100px;
+    height: 100px;
+    background: ${props => props.bgc || 'blue'};
+`
+// 定义关键帧
+const move = keyframes`
+    0% {
+        transform: rotate(45deg)
+    }
+    100% {
+        transform: rotate(-45deg)
+    }
+`
+// 使用动画
+const Box5 = styled.div`
+    width: 10px;
+    height: 100px;
+    background: red;
+    margin: 20px auto;
+    transform-origin: center bottom;
+    animation: ${move} 200ms infinite alternate linear;
+`
+
+export {
+    Box1,
+    Box2,
+    Box3,
+    Box4,
+    Box5
+}
