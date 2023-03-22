9b4a4917e7d0766076f13ff0f7d3bb2ef7e133ca
张宗义 <2293295470@qq.com>
2023-03-22 17:30:21 +0800

    aa

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App(Props) {
+    return (
+        <div>
+            <h5>{Props.title}</h5>
+            <h5>{Props.subTitle }</h5>
+        </div>
+    )
+}
