407a9d49df599625494730fef272fd97c41b31f1
张宗义 <2293295470@qq.com>
2023-03-22 19:56:52 +0800

    yy

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import React, { Component } from 'react'
 import './style/reset.css'
 import './style/main.css'
 import './style/ccc.scss'
+import styles from './style/App.module.css'
 
 export default class App extends Component {
     render() {
         return (
             <div>
+                <div className={styles.c}>css modules</div>
+
                 <div className='box1' style={{width: 100,height: 100,backgroundColor: 'pink'}}></div>
 
                 <div className='box2'></div>
