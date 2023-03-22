42ff16121506c56fe9d6577e167f5f2b06e91040
张宗义 <2293295470@qq.com>
2023-03-22 17:52:31 +0800

    ss

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import React, { Component } from 'react'
 export default class App extends Component {
     render() {
         return (
-        <div>
-            class组件
-            <h5>{this.props.title}</h5>
-            <h5>{this.props.subTitle}</h5>
-        </div>
-        
+            React.createElement('div', {className:'box', id: 'wrap'}, [
+                React.createElement('p', {className: 'op'}, ['这是p']),
+                React.createElement('span', {}, ['这是span']),
+                '这是文本内容--jsx原理'
+            ])
+            
         )
     }
     }
