48a38a9eb95cab324117817047065709e93ccc3b
张宗义 <2293295470@qq.com>
2023-03-23 17:24:43 +0800

    ddd

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,11 @@
 import React, { Component } from 'react'
+import Todo from './Todo'
 
 export default class App extends Component {
-    state = {
-        arr: [1, 2, 3, 4, 5, 6, 7]
-    }
     render() {
         return (
             <div>
-                <ul>
-                    {this.state.arr.map((el, index) => {
-                        return (
-                            <li key={el}>
-                                {el}
-                            </li>
-                        )
-                    })}
-                </ul>
+                <Todo/>
             </div>
         )
     }
---
+++ src/Todo.jsx
@@ -0,0 +1,9 @@
+import React, { Component, Fragment } from 'react'
+
+export default class Todo extends Component {
+    render() {
+        return (
+            <Fragment>Todo</Fragment>
+        )
+    }
+}
