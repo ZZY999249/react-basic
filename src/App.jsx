cf155758f004fd1b27dc736076127ae8fa5b6778
张宗义 <2293295470@qq.com>
2023-03-27 09:41:51 +0800

    dddd

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,25 @@ import React, { Component } from 'react'
 import Todo from './Todo'
 
 export default class App extends Component {
-    state={
+    state = {
         isShow: true
     }
     render() {
         return (
             <div id='app'>
-                <button onClick={()=>{
+                <button onClick={() => {
                     this.setState({
                         isShow: !this.state.isShow
                     })
                 }}>
-                    {this.state.isShow? '隐藏': '显示'}
+                    {this.state.isShow ? '隐藏' : '显示'}
                 </button>
                 {
                     this.state.isShow
                     &&
-                    <Todo/>
+                    <Todo num={10} />
                 }
-                
+
             </div>
         )
     }
--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,89 +1,27 @@
-import React, { Component, createRef } from 'react'
-import axios from 'axios'
-import * as echarts from 'echarts'
-import './style/style.css'
+import React, { Component } from 'react'
 
 export default class Todo extends Component {
-    constructor() {
-        super()
-        this.state = {
-            cates: [],
-            chart: null,
-            timer: null
+    state={
+        msg: 'i am you'
+    }
+    static getDerivedStateFromProps(props, state){
+        return {
+            doubleNum: props.num * 2,
+            reverseMsg: state.msg.split('').reverse().join('')
         }
-        this.chartRef = createRef()
     }
     render() {
+        console.log(this);
         return (
             <div>
-                <ul>
-                    {
-                        this.state.cates.map(cate => (
-                            <li key={cate.id}>
-                                <img src={cate.icon} alt="" />
-                                <h6>{cate.name}</h6>
-                            </li>
-                        ))
-                    }
-                </ul>
-                <div ref={this.chartRef} className='charts'></div>
+                {this.props.num}
+                <br />
+                {this.state.doubleNum}
+                <hr />
+                {this.state.msg}
+                <br />
+                {this.state.reverseMsg}
             </div>
         )
     }
-    fetchCates() {
-        axios.get("https://api.it120.cc/conner/cms/category/list").then(res => {
-            if (res.data.code === 0) {
-                this.setState({
-                    cates: res.data.data
-                })
-            }
-        })
-    }
-    initCharts() {
-        const chart = echarts.init(this.chartRef.current);
-        // setOption 绘制图表
-        const options = {
-            title: {
-                text: 'ECharts 入门示例'
-            },
-            tooltip: {},
-            xAxis: {
-                data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
-            },
-            yAxis: {},
-            series: [
-                {
-                    name: '销量',
-                    type: 'bar',
-                    data: [5, 20, 36, 10, 10, 20]
-                }
-            ]
-        }
-        chart.setOption(options)
-        this.setState({
-            chart
-        })
-    }
-    componentDidMount() {
-        this.fetchCates()
-
-        this.initCharts()
-
-        window.onscroll = () => {
-            console.log('页面滚动了');
-        }
-        const timer = setInterval(() => {
-            console.log('定时器');
-        }, 1000)
-        this.setState({
-            timer
-        })
-    }
-    componentWillUnmount(){
-        console.log('组件将要卸载');
-        window.onscroll = null
-        clearInterval(this.state.timer)
-    }
-
-
 }
